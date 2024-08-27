2b3c8e840ef6cf0464bf4c9534bd0d0881cec389
Rajib Ha San <rajibofficial1999@gmail.com>
2024-08-27 14:25:05 +0600

    final adding

--- app/Services/MegapersonalsService.php
+++ app/Services/MegapersonalsService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Account;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
 
 class MegapersonalsService
 {
@@ -15,12 +16,14 @@ class MegapersonalsService
         // remove url query '?bad_captcha' if already exists
         $previousUrl = Collection::make(explode('?bad_captcha', url()->previous()))->first();
 
-        if(!in_array($data["captcha"], $captchaCodes)) {
+        if(Arr::exists($data, 'captcha')){
+            if(!in_array($data["captcha"], $captchaCodes)) {
 
-            return [
-                'success' => false,
-                'redirect_url'=> "$previousUrl?bad_captcha=The Captcha code does not match."
-            ];
+                return [
+                    'success' => false,
+                    'redirect_url'=> "$previousUrl?bad_captcha=The Captcha code does not match."
+                ];
+            }
         }
 
         $account = Account::create($data);
